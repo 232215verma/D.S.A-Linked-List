377770e9f19cc2d6e0554731bc1aa1918d7da07c
232215verma <232215@kit.ac.in>
2026-08-09 10:05:37 +0530

    leetcode 142

---
+++ Linked list cycle 2.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+    ListNode* slow=head;
+    ListNode* fast=head;
+    bool iscycle=false;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            iscycle=true;
+            break;//becuase we preserve the fast and slow pointer;
+        }
+    }
+    if(!iscycle){
+        return NULL;
+    }
+    slow=head;
+    while(slow!=fast){
+        slow=slow->next;
+        fast=fast->next;
+    }
+    return slow;
+    }
+};
